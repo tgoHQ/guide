dfaa8d97700dc62d851a7e5a6b96da1d79d4cf42
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-12-01 23:33:49 -0500

    rm pwa stuff, move some deps to devdependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 import metaTags from 'astro-meta-tags';
 import starlightImageZoomPlugin from 'starlight-image-zoom';
-import AstroPWA from '@vite-pwa/astro';
 import inoxToolsStarWarp from '@inox-tools/star-warp';
 
 // https://astro.build/config
@@ -13,58 +12,6 @@ export default defineConfig({
 		prefetchAll: true,
 	},
 	integrations: [
-		// AstroPWA({
-		// 	mode: 'production',
-		// 	registerType: 'autoUpdate',
-		// 	includeAssets: ['favicon.svg'],
-		// 	workbox: {
-		// 		globPatterns: ['**/!(404).{css,js,html,svg,png,ico,txt,json}'],
-		// 		runtimeCaching: [
-		// 			{
-		// 				urlPattern: '.*',
-		// 				handler: 'NetworkFirst',
-		// 			},
-		// 		],
-		// 	},
-		// 	experimental: {
-		// 		directoryAndTrailingSlashHandler: true,
-		// 	},
-		// 	manifest: {
-		// 		id: 'the-great-outdoors',
-		// 		name: 'The Great Outdoors',
-		// 		short_name: 'TGO',
-		// 		description: 'Your guide to camping, hiking, and backpacking.',
-		// 		orientation: 'portrait',
-		// 		display: 'standalone',
-		// 		theme_color: '#137c5a',
-		// 		background_color: '#222222',
-		// 		icons: [
-		// 			{
-		// 				src: 'pwa-64x64.png',
-		// 				sizes: '64x64',
-		// 				type: 'image/png',
-		// 			},
-		// 			{
-		// 				src: 'pwa-192x192.png',
-		// 				sizes: '192x192',
-		// 				type: 'image/png',
-		// 			},
-		// 			{
-		// 				src: 'pwa-512x512.png',
-		// 				sizes: '512x512',
-		// 				type: 'image/png',
-		// 				purpose: 'any',
-		// 			},
-		// 			{
-		// 				src: 'maskable-icon-512x512.png',
-		// 				sizes: '512x512',
-		// 				type: 'image/png',
-		// 				purpose: 'maskable',
-		// 			},
-		// 		],
-		// 	},
-		// }),
-
 		starlight({
 			lastUpdated: true,
 			plugins: [starlightImageZoomPlugin(), inoxToolsStarWarp()],
